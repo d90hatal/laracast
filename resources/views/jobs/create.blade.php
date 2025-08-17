354619dd8362a053c4863d845be9bb6bb33236c3
david <hilelit4321@gmail.com>
2025-08-17 23:37:50 +0300

    server validation

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -16,20 +16,36 @@
                         <div class="mt-2">
                             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <div class="shrink-0 text-base text-gray-500 select-none sm:text-sm/6"></div>
-                                <input id="title" type="text" name="title" placeholder="manager" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                                <input id="title" type="text" name="title" placeholder="manager" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required>
                             </div>
                         </div>
+                        @error('title')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="sm:col-span-4">
                         <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
                         <div class="mt-2">
                             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <div class="shrink-0 text-base text-gray-500 select-none sm:text-sm/6"></div>
-                                <input id="salary" type="text" name="salary" placeholder="100000 per year" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                                <input id="salary" type="text" name="salary" placeholder="100000 per year" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required>
+                                
                             </div>
                         </div>
+                        @error('salary')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
+                <!-- @if($errors->any())
+                    <div class="mt-6 text-sm text-red-600">
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif -->
             </div>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,12 @@ Route::get('/contact', function () {
 });
 
 Route::post('/jobs', function () {
+    
+    request()->validate([
+        'title' => 'required | min:3',
+        'salary' => 'required',
+    ]);
+
    Job::create([
     'title' => request('title'),
     'salary' => request('salary'),
